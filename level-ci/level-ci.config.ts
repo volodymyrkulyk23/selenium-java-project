a6625cf68afb545fd2b080f5d6c331c5a05b6b1a
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2026-06-11 14:15:06 +0300

    br1 - overall scan

--- level-ci/level-ci.config.ts
+++ level-ci/level-ci.config.ts
@@ -3,5 +3,12 @@ export default {
  organization: "volodymyr-kulyk-1-levelaccess-com-clkuq",
  project: "selenium-java-project",
  token: process.env.LEVEL_CI_TOKEN,
+ override: {
+    "br1": {
+      targetBranch: "main",
+      retention: "long",
+      scope: "overall",
+    },
+  },
  reportPaths: ['./level-ci/level-ci-reports']
 } satisfies Config;
