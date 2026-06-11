11634dc4b52fd66facaeeffa80fc135b36702b82
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2026-06-11 14:01:17 +0300

    main - initial scan

--- level-ci/level-ci.config.ts
+++ level-ci/level-ci.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from "@level-ci/cli";
 export default {
- organization: "volodymyr-kulyk-40-userway-org-lando",
+ organization: "volodymyr-kulyk-1-levelaccess-com-clkuq",
  project: "selenium-java-project",
  token: process.env.LEVEL_CI_TOKEN,
- server: 'https://dev.uw.ci.levelaccess.io/',
  reportPaths: ['./level-ci/level-ci-reports']
 } satisfies Config;
