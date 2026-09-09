5c77a6ad245daba9e83e63b85a5d2b46382384ef
volodymyrkulyk23 <volodymyr.kulyk@userway.org>
2026-09-09 13:38:44 +0300

    Java project - baseline scan

--- level-ci/level-ci.config.ts
+++ level-ci/level-ci.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@level-ci/cli";
 export default {
  organization: "volodymyr-kulyk-1-levelaccess-com-clkuq",
- project: "selenium-java-project",
+ project: "selenium-java-project-1",
  token: process.env.LEVEL_CI_TOKEN,
  reportPaths: ['./level-ci/level-ci-reports']
-} satisfies Config;
+} satisfies Config; 
